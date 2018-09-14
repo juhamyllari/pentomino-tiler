06e79368175121d52a998d5058f7371d4ebc5886
Juha Myllari <jmyllari@gmail.com>
2018-09-14 21:35:24 +0300

    punctuation

--- PentominoTiler/src/main/java/fi/tira/pentominotiler/logic/PieceUtils.java
+++ PentominoTiler/src/main/java/fi/tira/pentominotiler/logic/PieceUtils.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A utility class for handling ArrayPiece objects
+ * A utility class for handling ArrayPiece objects.
  * @author juha
  */
 public class PieceUtils {
