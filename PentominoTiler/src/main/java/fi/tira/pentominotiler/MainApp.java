961a8ea2723a0e82474370bdaf3325ea523cae06
Juha Myllari <jmyllari@gmail.com>
2018-09-28 19:22:32 +0300

    uncomment line to show solutions

--- PentominoTiler/src/main/java/fi/tira/pentominotiler/MainApp.java
+++ PentominoTiler/src/main/java/fi/tira/pentominotiler/MainApp.java
@@ -24,8 +24,10 @@ public class MainApp extends Application {
         /*
         The GUI does not do anything interesting yet. This is a testing ground for elements of the program logic.
          */
-        Search mySearch = new Search(new Board(3, 20, Board.LETTER_SYMBOLS));
+        Search mySearch = new Search(new Board(6, 10, Board.LETTER_SYMBOLS));
         mySearch.runSearch();
+        
+        //Uncomment the line below to show all solutions.
 //        mySearch.getSolutions().forEach(b -> System.out.print(b.printableBoard()));
         
         /*
