4f79d7bb9171cbb9a1f774a7b13e61064ce6c712
Juha Myllari <jmyllari@gmail.com>
2018-10-20 15:50:25 +0300

    new unit tests

--- PentominoTiler/src/test/java/fi/tira/pentominotiler/datastructures/MyArrayListTest.java
+++ PentominoTiler/src/test/java/fi/tira/pentominotiler/datastructures/MyArrayListTest.java
@@ -12,22 +12,22 @@ import static org.junit.Assert.*;
  * @author juha
  */
 public class MyArrayListTest {
-    
+
     public MyArrayListTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -39,7 +39,7 @@ public class MyArrayListTest {
         lst.add(0);
         assertEquals(true, lst.contains(0));
         assertEquals(false, lst.contains(1));
-        
+
     }
 
     @Test
@@ -64,5 +64,93 @@ public class MyArrayListTest {
         }
         assertEquals(10, lst.size());
     }
-    
+
+    @Test
+    public void testIsEmpty() {
+        MyArrayList<Integer> lst = new MyArrayList<>();
+        assertEquals(true, lst.isEmpty());
+        lst.add(-23);
+        assertEquals(false, lst.isEmpty());
+    }
+
+    @Test
+    public void testContains() {
+        MyArrayList<Integer> lst = new MyArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            lst.add(i);
+        }
+        assertEquals(true, lst.contains(0));
+        assertEquals(true, lst.contains(9));
+        assertEquals(false, lst.contains(10));
+    }
+
+    @Test
+    public void testIterator() {
+        MyArrayList<Integer> lst = new MyArrayList<>();
+        assertEquals(false, lst.iterator().hasNext());
+        lst.add(-1);
+        assertEquals(true, lst.iterator().hasNext());
+    }
+
+    @Test
+    public void testAddAll() {
+        MyArrayList<Integer> lst1 = new MyArrayList<>();
+        MyArrayList<Integer> lst2 = new MyArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            lst1.add(i);
+        }
+        lst2.addAll(lst1);
+        assertEquals(10, lst2.size());
+
+    }
+
+    /**
+     * Will not be implemented.
+     */
+    @Test
+    public void testClear() {
+    }
+
+    /**
+     * Will not be implemented.
+     */
+    @Test
+    public void testToArray_0args() {
+    }
+
+    /**
+     * Will not be implemented.
+     */
+    @Test
+    public void testToArray_GenericType() {
+    }
+
+    /**
+     * Will not be implemented.
+     */
+    @Test
+    public void testRemove() {
+    }
+
+    /**
+     * Will not be implemented.
+     */
+    @Test
+    public void testContainsAll() {
+    }
+
+    /**
+     * Will not be implemented.
+     */
+    @Test
+    public void testRemoveAll() {
+    }
+
+    /**
+     * Will not be implemented.
+     */
+    @Test
+    public void testRetainAll() {
+    }
+
 }
